90cd59872191953b5e37e17488687f8678cebcc5
quimaguirre <quim.aguirre@hotmail.com>
2021-07-25 01:51:35 +0200

    Update to Python3

--- OboParser.py
+++ OboParser.py
@@ -17,34 +17,34 @@ class Relationship:
             self.name = " ".join(parts[4:])
         elif tag_line.startswith('is_a'):
             parts = tag_line.split(' ')
-	    self.type = parts[0].rstrip(':')
+            self.type = parts[0].rstrip(':')
             self.id = parts[1]
             self.name = " ".join(parts[3:])
         elif save_synonyms and tag_line.startswith('synonym'):
-	    temp = re.search("\"(.+)\"\s+(\w+)",tag_line)
-	    #if temp.group(2) == "EXACT": #synonym = temp.group(1)
+            temp = re.search("\"(.+)\"\s+(\w+)",tag_line)
+            #if temp.group(2) == "EXACT": #synonym = temp.group(1)
             self.type = "synonym" # synonym
             self.id = temp.group(1) # synonymous term
             self.name = temp.group(2) # synonym type
-	elif tag_line.startswith("subset"):
-	    self.type = "subset"
-	    self.subset = tag_line.split(' ')[1]
-	elif tag_line.startswith("is_obsolete"):
-	    self.type = "obsolete"
-	    self.subset = tag_line.split(' ')[1]
+        elif tag_line.startswith("subset"):
+            self.type = "subset"
+            self.subset = tag_line.split(' ')[1]
+        elif tag_line.startswith("is_obsolete"):
+            self.type = "obsolete"
+            self.subset = tag_line.split(' ')[1]
         elif tag_line.startswith('alt_id'):
             parts = tag_line.split(' ')
-	    self.type = parts[0].rstrip(':')
+            self.type = parts[0].rstrip(':')
             self.id = parts[1]
         elif tag_line.startswith('xref'):
             parts = tag_line.split(' ')
-	    self.type = parts[0].rstrip(':')
+            self.type = parts[0].rstrip(':')
             self.id = parts[1]
-	else:
-	    self.type = "ignore"
+        else:
+            self.type = "ignore"
         
     def __str__(self):
-        return "relationship: %s %s ! %s"%(self.type, self.id, self.name)
+        return "relationship: {} {} ! {}".format(self.type, self.id, self.name)
 
 
 def getOboGraph(fname, save_synonyms = False, exclude_obsolete = True):
@@ -74,33 +74,33 @@ def getOboGraph(fname, save_synonyms = False, exclude_obsolete = True):
                 stanza = obo_file.next().strip()
                 if stanza=='':
                     break
-		# Namespace 
-		if stanza.startswith("namespace"):
-		    namespace = stanza[11:]
-		    obo.node[id]['t'] = namespace
-		    continue
+                # Namespace 
+                if stanza.startswith("namespace"):
+                    namespace = stanza[11:]
+                    obo.node[id]['t'] = namespace
+                    continue
                 stanza = Relationship(stanza, save_synonyms)
-		if stanza.type == "ignore":
-		    continue
-		elif stanza.type == "subset":
-		    if stanza.subset == "goslim_yeast":
-			obo.node[id]['y'] = True
-		    if stanza.subset.startswith("goslim"):
-			obo.node[id]['a'] = True
-		elif stanza.type == "obsolete":
-		    if exclude_obsolete:
-			continue
-		    else:
-			obo.node[id]['o'] = True
-		# Synonyms 
-		elif save_synonyms and stanza.type == "synonym":
-		    obo.node[id].setdefault('s', set()).add(stanza.id)
-		    #obo.node[id]['s'].add(stanza.id)
-		elif stanza.type in ("alt_id", "xref"):
-		    obo.node[id][stanza.type].append(stanza.id)
-		elif stanza.type in ("is_a", "relationship"): 
-		    obo.add_edge(id, stanza.id)
-		    obo[id][stanza.id]['r'] = stanza.type
+                if stanza.type == "ignore":
+                    continue
+                elif stanza.type == "subset":
+                    if stanza.subset == "goslim_yeast":
+                        obo.node[id]['y'] = True
+                    if stanza.subset.startswith("goslim"):
+                        obo.node[id]['a'] = True
+                elif stanza.type == "obsolete":
+                    if exclude_obsolete:
+                        continue
+                    else:
+                        obo.node[id]['o'] = True
+                # Synonyms 
+                elif save_synonyms and stanza.type == "synonym":
+                    obo.node[id].setdefault('s', set()).add(stanza.id)
+                    #obo.node[id]['s'].add(stanza.id)
+                elif stanza.type in ("alt_id", "xref"):
+                    obo.node[id][stanza.type].append(stanza.id)
+                elif stanza.type in ("is_a", "relationship"): 
+                    obo.add_edge(id, stanza.id)
+                    obo[id][stanza.id]['r'] = stanza.type
     
     return obo
 

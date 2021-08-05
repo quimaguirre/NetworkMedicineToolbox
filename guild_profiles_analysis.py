91c1eed002581bb551e838d76a4ffe5dbe4597ea
quimaguirre <quim.aguirre@hotmail.com>
2021-08-05 23:00:55 +0200

    Include profile manager of GUILD from DIANA

--- guild_profiles_analysis.py
+++ guild_profiles_analysis.py
@@ -3,8 +3,7 @@ import networkx as nx
 import numpy as np
 import scipy.stats
 import os, sys
-
-import diana.classes.top_scoring as TOP
+from . import network_utilities
 
 """
     NetworkAnalysis
@@ -881,6 +880,19 @@ class IncorrectTypeID(Exception):
     def __str__(self):
         return 'The initial type of IDs of the network is not biana, it is {}. It is only possible to translate from BIANA codes'.format(self.type_id)
 
+class IncorrectThresholdType(NameError):
+    """
+    Subclass exception of the NameError which raises when a network format is
+    not provided correctly
+    """
+    def __init__(self, wrong_top_type, top_types):
+        self.wrong_top_type = wrong_top_type
+        self.top_types = top_types
+
+    def __str__(self):
+        return 'The threshold type {} is not valid. It must be one of the following threshold types: {}'.format(self.wrong_top_type, self.top_types)
+
+
 
 ###################
 #### FUNCTIONS ####
@@ -890,6 +902,7 @@ def calculate_contingency_table(contingency_table):
     """Gets a complete network and filters by tissue interactions in the same tissue"""
     chi2, pval, dof, expected = scipy.stats.chi2_contingency(contingency_table)
     return chi2, pval, dof, expected
+
 
 def write_network_file_from_networkx_graph(input_network, output_network_file, output_nodes_file, output_network_format, tissue_specific=False):
     """
@@ -928,25 +941,14 @@ def write_network_file_from_networkx_graph(input_network, output_network_file, o
 
     return
 
+
 def get_nodes_intersection_of_two_networks(network1, network2):
     """Gets the intersecting nodes of two networks"""
     network1_nodes = set(network1.get_nodes())
     network2_nodes = set(network2.get_nodes())
     return network1_nodes & network2_nodes
 
-# def get_edges_intersection_of_two_networks(network1, network2):
-#     """Gets the intersecting edges of two networks"""
-#     intersection = set()
-#     network2_edges = network2.get_edges()
-#     for u,v in network1.network.edges_iter():
-#         comb1 = (u,v)
-#         comb2 = (v,u)
-#         if comb1 in network2_edges or comb2 in network2_edges:
-#             if comb1 in network2_edges:
-#                 intersection.add(comb1)
-#             else:
-#                 intersection.add(comb2)
-#     return intersection
+
 def get_edges_intersection_of_two_networks(network1, network2):
     """Gets the intersecting edges of two networks"""
     network1_edges = network1.get_edges()
@@ -956,3 +958,150 @@ def get_edges_intersection_of_two_networks(network1, network2):
     network2_set = set(frozenset(i) for i in network2_edges)
 
     return network1_set & network2_set
+
+
+def node_top_scoring(node_to_score, threshold, threshold_type, output_file, verbose=False):
+    """
+    Creates a profile with the most relevant nodes using a percentage threshold of the score provided by the user.
+
+    Parameters:
+        @node_to_score:         Dictionary of all the nodes to their GUILD score
+        @threshold:             Top percentage / number of nodes of the scores file in which we will cut to obtain the most relevant nodes
+        @threshold_type:        Type of threshold used to define the top scoring nodes (it can either be 'percentage' or 'number_of_nodes')
+        @output_file:           Resulting file which will contain the most relevant nodes.
+    """
+
+    # Get number of top scoring nodes
+    if threshold_type == 'percentage':
+        # By calculating the percentage over the total
+        ntop=round(float(threshold)*len([x for x in node_to_score.items()])/100.0)
+    elif threshold_type == 'number_of_nodes':
+        # By exact number of nodes
+        ntop=int(threshold)
+    else:
+        raise TOP.IncorrectThresholdType(wrong_top_type=threshold_type, top_types=['percentage', 'number_of_nodes'])
+
+    # Now, write the profile with the top scoring nodes
+    top_nodes = set()
+    last_score = ''
+    ii=0
+    with open(output_file, 'w') as f:
+        f.write('#Id\tScore\n')
+        for node, score in sorted(node_to_score.items(),key=lambda x: x[1],reverse=True):
+            if ii < ntop:
+                top_nodes.add(node)
+                last_score = score # The last score is saved, so that if there is a score which is above the top threshold but has the same value than the last node, it is included as well                    
+                f.write('{}\t{}\n'.format(node, score))
+                ii=ii+1
+            else:
+                if score == last_score: # Here, if a score above the threshold has the same score as the last, it is also recorded
+                    top_nodes.add(node)
+                    f.write('{}\t{}\n'.format(node, score))
+                else:
+                    break
+
+    if verbose:
+        print('INFO:\t{} file created.\n'.format(output_file))
+    return
+
+
+def edge_top_scoring(network_file, node_to_score, threshold, threshold_type, output_file, verbose=False):
+    """
+    Creates profiles with the most relevant edges using the thresholds provided by the user.
+    The profile is created selecting the top most relevant nodes, and creating the subsequent scored subnetwork.
+
+    Parameters:
+        @network_file:          File containing the scored network
+        @node_to_score:         Dictionary of all the nodes to their GUILD score
+        @threshold:             Top percentage of the network in which we will cut to obtain the most relevant edges
+        @threshold_type:        Type of threshold used to define the top scoring nodes (it can either be 'percentage' or 'number_of_nodes')
+        @output_file:           Resulting file which will contain the most relevant edges
+    """
+
+    # Create a network from the network file
+    g = network_utilities.create_network_from_sif_file(network_file, use_edge_data=True)
+
+    # Get number of top scoring nodes
+    if threshold_type == 'percentage':
+        # By calculating the percentage over the total
+        ntop=round(float(threshold)*len([x for x in node_to_score.items()])/100.0)
+    elif threshold_type == 'number_of_nodes':
+        # By exact number of nodes
+        ntop=int(threshold)
+    else:
+        raise TOP.IncorrectThresholdType(wrong_top_type=threshold_type, top_types=['percentage', 'number_of_nodes'])
+
+    top_nodes = set()
+    ii=0
+    last_score = ''
+
+    # Filter the nodes by top scoring nodes
+    for node, score in sorted(node_to_score.items(),key=lambda x: x[1],reverse=True):
+        if ii < ntop:
+            top_nodes.add(node)
+            last_score = score # The last score is saved, so that if there is a score which is above the top threshold but has the same value than the last node, it is included as well
+            ii=ii+1
+        else:
+            if score == last_score: # Here, if a score above the threshold has the same score as the last, it is also recorded
+                top_nodes.add(node)
+            else:
+                break
+
+    # Get subnetwork induced by top scoring nodes
+    g_sub = get_subgraph(g, top_nodes)
+
+    with open(output_file, 'w') as f:
+
+        # The subnetwork induced by the top scoring nodes is scored and recorded in a file
+        for u, v in g_sub.edges():
+            score_u = node_to_score[u]
+            score_v = node_to_score[v]
+            score = (score_u + score_v) / 2
+            f.write('{}\t{:f}\t{}\n'.format(u, score, v))
+
+    if verbose:
+        print('INFO:\t{} file created.\n'.format(output_file))
+    return
+
+
+def functional_top_scoring(top_geneids, type_correction, associations_file, output_file, verbose=False):
+    """
+    Creates profiles with the most relevant functions using the thresholds provided by the user.
+    The profile is created selecting the top most relevant nodes, and computing a functional enrichment analysis of them.
+
+    Parameters:
+        @top_geneids:           A list containing the selected Entrez gene IDs to do the enrichment analysis
+        @type_correction:       Type of p-value multiple test correction (fda_bh or bonferroni)
+        @output_file:           Resulting file which will contain the functions enriched.
+        @associations_file:     File containing the function-gene associations
+    """
+    FA.calculate_functional_enrichment_profile(top_geneids=top_geneids, type_correction=type_correction, associations_file=associations_file, output_file=output_file)
+    if verbose:
+        print('INFO:\t{} file created.\n'.format(output_file))
+    return
+
+
+def score_network(network, node_to_score, output_file):
+    """
+    Scores a network
+    """
+
+    # Get all nodes
+    all_nodes = node_to_score.keys()
+
+    # Here, the network is scored and stored in a file
+    with open(output_file, 'w') as f:
+        for u, v in network.edges():
+            score_u = node_to_score[u]
+            score_v = node_to_score[v]
+            score = (float(score_u) + float(score_v)) / 2
+            f.write('{}\t{:f}\t{}\n'.format(u, score, v))
+ 
+    return
+
+
+def get_subgraph(G, nodes):
+    """
+    NetworkX subgraph method wrapper
+    """
+    return G.subgraph(nodes)

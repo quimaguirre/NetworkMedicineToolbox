c22e39286ae1c4ec1ae4365cf6c2e15380d61dd2
Quim Aguirre <quim.aguirre@hotmail.com>
2022-03-15 14:28:05 -0400

    Changes in NCBI gene parser

--- parse_ncbi.py
+++ parse_ncbi.py
@@ -1,103 +1,222 @@
-
-from . import TsvReader
-
 def main():
     return
 
 def get_geneid_symbol_mapping(file_name):
     """
-    To parse Homo_sapiens.gene_info (trimmed to two colums) file from NCBI 
-    Creating the file
-    wget ftp://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz
-    zcat Homo_sapiens.gene_info.gz | cut -f 2,3,5 > geneid_to_symbol.txt
-    Remove first line if need be (but keep header)
+    Parses Homo_sapiens.gene_info from NCBI. 
+    The file can be downloaded at: https://ftp.ncbi.nlm.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz
     """
-    f = open(file_name)
-    f.readline()
     geneid_to_name = {} # now contains only the official symbol
     name_to_geneid = {}
-    for line in f:
-        words = line.strip("\n").split("\t")
-        if len(words) == 2:
-            geneid, symbol = words
-        else:
-            tax_id, geneid, symbol, locus, alternatives = words[:5]
-            alternatives = alternatives.split("|")
-        geneid = geneid.strip() # strip in case mal formatted input file
-        symbol = symbol.strip()
-        if geneid == "" or symbol == "":
-            continue
-        #geneid_to_names.setdefault(geneid, set()).add(symbol) 
-        geneid_to_name[geneid] = symbol
-        for symbol in [symbol] + alternatives: # added for synonym parsing
-            if symbol in name_to_geneid: 
-                if int(geneid) >= int(name_to_geneid[symbol]):
-                    continue
-                print("Multiple geneids", name_to_geneid[symbol], geneid, symbol)
-            name_to_geneid[symbol] = geneid
-    f.close()
-    return geneid_to_name, name_to_geneid
-
-
-def get_unigene_to_geneids(file_name, prefix = "Hs."):
-    """
-    To parse gene2unigene file from NCBI
-    """
-    f = open(file_name)
-    unigene_to_geneids = {}
-    f.readline()
-    for line in f:
-        geneid, unigene = line.strip().split("\t")
-        if not unigene.startswith(prefix):
-            continue
-        unigene_to_geneids.setdefault(unigene, set()).add(geneid)
-    #for unigene, geneids in unigene_to_geneids.iteritems():
-    #        if len(geneids) > 1:
-    #            print(unigene, geneids)
-    return unigene_to_geneids
+    geneid_to_synonyms = {}
+    with open(file_name) as f:
+        for line in f:
+            words = line.strip("\n").split("\t")
+            if len(words) == 2:
+                geneid, symbol = words
+            else:
+                tax_id, geneid, symbol, locus, alternatives = words[:5]
+                alternatives = alternatives.split("|")
+            geneid = geneid.strip() # strip in case mal formatted input file
+            symbol = symbol.strip()
+            if geneid == "" or symbol == "":
+                continue
+            #geneid_to_names.setdefault(geneid, set()).add(symbol) 
+            geneid_to_name[geneid] = symbol
+            for alternative in alternatives:
+                geneid_to_synonyms.setdefault(geneid)
+                geneid_to_synonyms[geneid].add(alternative)
+            for symbol in [symbol] + alternatives: # added for synonym parsing
+                if symbol in name_to_geneid: 
+                    if int(geneid) >= int(name_to_geneid[symbol]):
+                        continue
+                    print("Multiple geneids", name_to_geneid[symbol], geneid, symbol)
+                name_to_geneid[symbol] = geneid
+    return geneid_to_name, name_to_geneid, geneid_to_synonyms
 
 
 def get_geneid_to_pubmeds(file_name, tax_id = "9606"):
     """
-    To parse gene2pubmed file from NCBI 
+    Parses gene2pubmed file from NCBI.
+    The file can be downloaded at: https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2pubmed.gz
     """
-    f = open(file_name)
     geneid_to_pubmeds = {}
-    f.readline()
-    for line in f:
-        tax, geneid, pubmed_id = line.strip().split("\t")
-        if tax != tax_id:
-            continue
-        geneid_to_pubmeds.setdefault(geneid, set()).add(pubmed_id)
+    with open(file_name) as f:
+        f.readline()
+        for line in f:
+            tax, geneid, pubmed_id = line.strip().split("\t")
+            if tax != tax_id:
+                continue
+            geneid_to_pubmeds.setdefault(geneid, set()).add(pubmed_id)
     return geneid_to_pubmeds
 
 
-def get_ensembl_protein_to_id(file_name, id_type="geneid"):
+def get_geneid_to_ensembl(file_name, id_type="geneid"):
     """
-    Get ENSEMBL mapping file from BioMart (exporting with ensembl transcipt / protein / gene / symbol / gene id fields)
-    id_type: geneid | genesymbol
+    Parses gene2ensembl file from NCBI.
+    The file can be downloaded at: https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2ensembl.gz
     """
-    ensembl_to_id = {}
-    parser = TsvReader.TsvReader(file_name, delim="\t")
-    fields_to_include = ["Protein stable ID"]
-    if id_type == "geneid":
-        column_name = "EntrezGene ID" #"NCBI gene ID" (in GRch38)
-    elif id_type == "genesymbol":
-        column_name = "HGNC symbol"
-    else:
-        raise ValueError("Unknown id type")
-    fields_to_include += [column_name]
-    column_to_index, id_to_values = parser.read(fields_to_include=fields_to_include)
-    for ensembl, values in id_to_values.iteritems():
-            for val in values:
-                gene = val[column_to_index[column_name.lower()]]
-            ensembl_to_id[ensembl] = gene
+    geneIDs = set()
+    geneid_to_taxid = {}
+    geneid_to_ensembl = {}
+    geneid_to_accession = {}
+
+    gene2ensembl_fd = gzip.open(gene2ensembl_file,'rb')
+
+    first_line = gene2ensembl_fd.readline()
+
+    # Obtain a dictionary: "field_name" => "position"
+    fields_dict = obtain_header_fields(first_line[1:])
+    #tax_id GeneID  Ensembl_gene_identifier RNA_nucleotide_accession.version    Ensembl_rna_identifier  protein_accession.version   Ensembl_protein_identifier
+
+    for line in gene2ensembl_fd:
+
+        # Split the line in fields
+        fields = line.strip().split("\t")
+
+        # Obtain the fields of interest
+        taxid = fields[ fields_dict['tax_id'] ]
+        geneid = fields[ fields_dict['GeneID'] ]
+        ensembl_gene = fields[ fields_dict['Ensembl_gene_identifier'] ]
+        accession_rna = fields[ fields_dict['RNA_nucleotide_accession.version'] ]
+        ensembl_rna = fields[ fields_dict['Ensembl_rna_identifier'] ]
+        accession_prot = fields[ fields_dict['protein_accession.version'] ]
+        ensembl_prot = fields[ fields_dict['Ensembl_protein_identifier'] ]
+
+        # Insert GeneID
+        geneIDs.add(geneid)
+
+        # Insert TaxID. There can only be one taxID for GeneID. If not, error
+        if geneid not in geneid_to_taxid:
+            geneid_to_taxid[geneid] = taxid
+        else:
+            if geneid_to_taxid[geneid] != taxid:
+                print('Different taxIDs for the GeneID: {}\nFirst taxID: {}  Second taxID: {}'.format(geneid, geneid_to_taxid[geneid], taxid))
+                sys.exit(10)
+
+        # Insert Ensembl
+        geneid_to_ensembl.setdefault(geneid, {})
+
+        if ensembl_gene != '-':
+            ensembl_gene = ensembl_gene.split('.')[0]
+            geneid_to_ensembl[geneid].setdefault('gene', [])
+            geneid_to_ensembl[geneid]['gene'].append(ensembl_gene)
+        if ensembl_rna != '-':
+            ensembl_rna = ensembl_rna.split('.')[0]
+            geneid_to_ensembl[geneid].setdefault('rna', [])
+            geneid_to_ensembl[geneid]['rna'].append(ensembl_rna)
+        if ensembl_prot != '-':
+            ensembl_prot = ensembl_prot.split('.')[0]
+            geneid_to_ensembl[geneid].setdefault('protein', [])
+            geneid_to_ensembl[geneid]['protein'].append(ensembl_prot)
+
+        # Insert Accession
+        geneid_to_accession.setdefault(geneid, {})
+
+        if accession_rna != '-':
+            geneid_to_accession[geneid].setdefault('rna', [])
+            geneid_to_accession[geneid]['rna'].append(accession_rna.split('.'))
+        if accession_prot != '-':
+            geneid_to_accession[geneid].setdefault('protein', [])
+            geneid_to_accession[geneid]['protein'].append(accession_prot.split('.'))
+
+    gene2ensembl_fd.close()
+
     return ensembl_to_id
+
+
+def get_geneid_to_go():
+    """
+    Parses gene2go file from NCBI.
+    The file can be downloaded at: https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2go.gz
+    """
+    geneIDs = set()
+    geneid_to_go = {}
+    geneid_to_go_to_evidences = {}
+    geneid_to_go_to_pubmeds = {}
+    geneid_to_taxid = {}
+
+    gene2go_fd = gzip.open(gene2go_file,'rb')
+    first_line = gene2go_fd.readline()
+
+    # Obtain a dictionary: "field_name" => "position"
+    fields_dict = obtain_header_fields(first_line[1:])
+    #tax_id GeneID  GO_ID   Evidence    Qualifier   GO_term PubMed  Category
+
+    for line in gene2go_fd:
+
+        # Split the line in fields
+        fields = line.strip().split("\t")
+
+        # Obtain the fields of interest
+        taxid = fields[ fields_dict['tax_id'] ]
+        geneid = fields[ fields_dict['GeneID'] ]
+        go_id = fields[ fields_dict['GO_ID'] ].upper()
+        evidence = fields[ fields_dict['Evidence'] ].upper()
+        qualifier = fields[ fields_dict['Qualifier'] ].lower()
+        go_term = fields[ fields_dict['GO_term'] ].lower()
+        pubmed = fields[ fields_dict['PubMed'] ]
+        category = fields[ fields_dict['Category'] ].lower()
+
+        # Insert GeneID
+        geneIDs.add(geneid)
+
+        # Insert TaxID. There can only be one taxID for GeneID. If not, error
+        if geneid not in geneid_to_taxid:
+            geneid_to_taxid[geneid] = taxid
+        else:
+            if geneid_to_taxid[geneid] != taxid:
+                print('Different taxIDs for the GeneID: {}\nFirst taxID: {}  Second taxID: {}'.format(geneid, geneid_to_taxid[geneid], taxid))
+                sys.exit(10)
+
+        # Insert GO
+        go_id = go_id.split('GO:')[1]
+        geneid_to_go.setdefault(geneid, set())
+        geneid_to_go[geneid].add(go_id)
+
+        if evidence != '-':
+            geneid_to_go_to_evidences.setdefault(geneid, {})
+            geneid_to_go_to_evidences[geneid].setdefault(go_id, set())
+            geneid_to_go_to_evidences[geneid][go_id].add(evidence)
+        else:
+            print('No evidence for GeneID {} and GO ID {}!'.format(geneid, go_id))
+            sys.exit(10)
+
+        if pubmed != '-':
+            geneid_to_go_to_pubmeds.setdefault(geneid, {})
+            geneid_to_go_to_pubmeds[geneid].setdefault(go_id, set())
+            geneid_to_go_to_pubmeds[geneid][go_id].add(pubmed)
+
+    gene2go_fd.close()
+
+    return geneid_to_go, geneid_to_go_to_evidences, geneid_to_go_to_pubmeds, geneid_to_taxid
+
+
+
+def get_unigene_to_geneids(file_name, prefix = "Hs."):
+    """
+    Parses gene2unigene file from NCBI.
+    The file can be downloaded at: https://ftp.ncbi.nlm.nih.gov/gene/DATA/ARCHIVE/gene2unigene
+    """
+    unigene_to_geneids = {}
+    with open(file_name) as f:
+        f.readline()
+        for line in f:
+            geneid, unigene = line.strip().split("\t")
+            if not unigene.startswith(prefix):
+                continue
+            unigene_to_geneids.setdefault(unigene, set()).add(geneid)
+        #for unigene, geneids in unigene_to_geneids.iteritems():
+        #        if len(geneids) > 1:
+        #            print(unigene, geneids)
+    return unigene_to_geneids
 
 
 def get_homology_mapping(file_name, tax_id, from_tax_id="9606", symbol_type="geneid"):
     """
-    file_name: Homologene data file
+    Parses Homologene and obtains the homology mappings from one species to another.
+    The file can be downloaded at: https://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data 
+    File_name: Homologene data file
     tax_id: Tax id of the species to the genes of which the mapping 
     will be done (e.g., to mouse genes, from human genes)
     from_tax_id: Tax id from which the mapping will be done (default is human)
@@ -121,14 +240,13 @@ def get_homology_mapping(file_name, tax_id, from_tax_id="9606", symbol_type="gen
         raise ValueError("Unknown symbol type: {}".format(symbol_type))
     # Parse group info
     group_to_taxid_to_geneid = {}
-    f = open(file_name)
-    for line in f:
-        words = line.strip("\n").split("\t")
-        group, taxid = words[:2]
-        geneid = words[idx]
-        d = group_to_taxid_to_geneid.setdefault(group, {})
-        d[taxid] = geneid
-    f.close()
+    with open(file_name) as f:
+        for line in f:
+            words = line.strip("\n").split("\t")
+            group, taxid = words[:2]
+            geneid = words[idx]
+            d = group_to_taxid_to_geneid.setdefault(group, {})
+            d[taxid] = geneid
     # Get geneid mapping
     geneid_to_geneid = {}
     for group, taxid_to_geneid in group_to_taxid_to_geneid.iteritems():
@@ -137,4 +255,16 @@ def get_homology_mapping(file_name, tax_id, from_tax_id="9606", symbol_type="gen
         geneid_to_geneid[taxid_to_geneid[from_tax_id]] = taxid_to_geneid[tax_id]
     return geneid_to_geneid, group_to_taxid_to_geneid 
 
- 
+
+def obtain_header_fields(first_line):
+    """ 
+    Obtain a dictionary: "field_name" => "position" 
+    """
+    fields_dict = {}
+
+    header_fields = first_line.strip().split("\t")
+    for x in xrange(0, len(header_fields)):
+        fields_dict[header_fields[x]] = x
+
+    return fields_dict
+
